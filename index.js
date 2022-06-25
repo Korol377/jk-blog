4b7d8c6875e64a037abb5e992e5efc23fb826288
Bohdan Korolevych <bogdan.korolevych2002@gmail.com>
2022-06-26 00:20:57 +0300

    user validation and password encryption

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
+import { validationResult } from 'express-validator';
+import { registerValidation } from './validations/auth.js';
+import UserModel from './models/User.js'
+
 
 mongoose
 .connect('mongodb+srv://admin:wwwwww@cluster0.zayboem.mongodb.net/?retryWrites=true&w=majority')
@@ -11,22 +16,31 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.post('/auth/register', registerValidation, async (req, res) => {
+    const errors = validationResult(req);
+    
 
-app.post('/auth/login/', (req, res) => {
-    console.log(req.body);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    }
 
-    const token = jwt.sign({
+    // Шифрування паролю 
+    const password = req.body.password;
+    // Алгоритм шифрування bcrypt
+    const salt = await  bcrypt.genSalt(10);
+    
+    const passwordHash = await bcrypt.hash(password, salt); 
+
+    const doc = new UserModel({
         email: req.body.email,
-        fullName: 'Bohdan Korolevych',
-    }, 'secret123');
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash
+    })
 
-    res.json({
-        success: true,
-        token,
-    });
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 app.listen(4444, (err) => {
